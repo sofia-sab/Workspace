f642460cef9ef41bc06308a4d6617fbc8aa3bfa3
sofia-sab <sofiasaboredo@gmail.com>
2024-08-22 18:47:27 -0300

    Archivo Json

---
+++ js/products.js
@@ -0,0 +1,8 @@
+// const data = "json/autos.json";
+// const contenedorJson = document.getElementById("contenedorJson");
+
+// function infoAutos(products){
+//     contenedorJson.innerHTML = '';
+//     for (const product of products)
+//     contenedorJson.innerHTML+='<p>{product.name} <br> ${}'
+// }
