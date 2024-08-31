1a154e6f2aea2216519d0a4edef489db57be613e
sofia-sab <sofiasaboredo@gmail.com>
2024-08-31 11:52:49 -0300

    Ingreso y muestra de usuario

--- js/autenticacion.js
+++ js/autenticacion.js
@@ -1,8 +1,10 @@
 function checkAuthentication() {
-    let username=localStorage.getItem('persona');
-    if (localStorage.getItem('persona') !== username) {
+    let username = localStorage.getItem('persona');
+    if(!username){
         window.location.href = 'login.html'; 
+    
+    } else{
+        document.getElementById('userShow').textContent=username;
     }
 }
-
 document.addEventListener('DOMContentLoaded', checkAuthentication);
