93f93c2ee17e10d1112eb003afef1e0c90d5fb71
sofia-sab <sofiasaboredo@gmail.com>
2024-09-10 18:18:56 -0300

    Acceder a todos los productos por categoria

--- js/products.js
+++ js/products.js
@@ -1,45 +1,100 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const catID = localStorage.getItem('catID'); 
 
-const data = 'https://japceibal.github.io/emercado-api/cats_products/101.json'; 
+    if (!catID) {
+        console.error('No se encontró el identificador de categoría en localStorage.');
+        return; 
+    }
+   
+const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
 
-fetch(data)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error en la red: ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const contenedorJson = document.getElementById('contenedorJson');
-        contenedorJson.innerHTML = ''; 
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la red: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const contenedorJson = document.getElementById('contenedorJson');
+            contenedorJson.innerHTML = ''; 
 
-        data.products.forEach(product => {
-            
-            const productoDiv = document.createElement('div');
-            productoDiv.className = 'producto';
-
-            
-            productoDiv.innerHTML = `
-        <div class="container-fluid">
-            <div class="row">
-                <div class= "col-xxl-3 col-md-6 col-xs-6 col-lg-4">
-                    <img class="auto" src="${product.image}" alt="${product.name}">
-                </div>
-                <div class= "col-xxl-9 col-md-6 col-xs-6 col-lg-8">
-                    <div class="informacion">
-                        <p class="negrita">${product.name}</p>
-                        <p>${product.description}</p>
-                        <p>Vendidos: ${product.soldCount}</p>
-                        <p class="precio">${product.currency} ${product.cost}</p>
+            data.products.forEach(product => {
+                const productoDiv = document.createElement('div');
+                productoDiv.className = 'producto';
+                
+                productoDiv.innerHTML = `
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class= "col-xxl-3 col-md-6 col-xs-6 col-lg-4">
+                            <img class="auto" src="${product.image}" alt="${product.name}">
+                        </div>
+                        <div class= "col-xxl-9 col-md-6 col-xs-6 col-lg-8">
+                            <div class="informacion">
+                                <p class="negrita">${product.name}</p>
+                                <p>${product.description}</p>
+                                <p>Vendidos: ${product.soldCount}</p>
+                                <p class="precio">${product.currency} ${product.cost}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </div>`;
+                
+                contenedorJson.appendChild(productoDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la solicitud:', error);
+        });
+});
+
+
+
+
+
+
+// const catID = localStorage.getItem('catID');
+// const data = 'https://japceibal.github.io/emercado-api/cats_products/${catID}.json'; 
+
+// fetch(data)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Error en la red: ' + response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         const contenedorJson = document.getElementById('contenedorJson');
+//         contenedorJson.innerHTML = ''; 
+
+//         data.products.forEach(product => {
             
-            </div>
-        </div>`;
+//             const productoDiv = document.createElement('div');
+//             productoDiv.className = 'producto';
 
             
-            contenedorJson.appendChild(productoDiv);
-        });
-    })
-    .catch(error => {
-        console.error('Hubo un problema con la solicitud:', error);
-    });
+//             productoDiv.innerHTML = `
+//         <div class="container-fluid">
+//             <div class="row">
+//                 <div class= "col-xxl-3 col-md-6 col-xs-6 col-lg-4">
+//                     <img class="auto" src="${product.image}" alt="${product.name}">
+//                 </div>
+//                 <div class= "col-xxl-9 col-md-6 col-xs-6 col-lg-8">
+//                     <div class="informacion">
+//                         <p class="negrita">${product.name}</p>
+//                         <p>${product.description}</p>
+//                         <p>Vendidos: ${product.soldCount}</p>
+//                         <p class="precio">${product.currency} ${product.cost}</p>
+//                     </div>
+//                 </div>
+            
+//             </div>
+//         </div>`;
+
+            
+//             contenedorJson.appendChild(productoDiv);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Hubo un problema con la solicitud:', error);
+//     });
