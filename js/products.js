f59352b89093c406eb023b359fa33176e5055c17
sofia-sab <sofiasaboredo@gmail.com>
2024-09-11 16:11:51 -0300

    Botones de filtro

--- js/products.js
+++ js/products.js
@@ -1,32 +1,10 @@
-function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
-            return 0;
-        })};
-    }
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const catID = localStorage.getItem('catID'); 
 
-
-    
-
-    if (!catID) {
-        console.error('No se encontró el identificador de categoría en localStorage.');
-        return; 
-    }
+    // if (!catID) {
+    //     console.error('No se encontró el identificador de categoría en localStorage.');
+    //     return; 
+    // }
 
 const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
 
@@ -71,52 +49,3 @@ const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.jso
 });
 
 
-
-
-
-
-// const catID = localStorage.getItem('catID');
-// const data = 'https://japceibal.github.io/emercado-api/cats_products/${catID}.json'; 
-
-// fetch(data)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Error en la red: ' + response.statusText);
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         const contenedorJson = document.getElementById('contenedorJson');
-//         contenedorJson.innerHTML = ''; 
-
-//         data.products.forEach(product => {
-            
-//             const productoDiv = document.createElement('div');
-//             productoDiv.className = 'producto';
-
-            
-//             productoDiv.innerHTML = `
-//         <div class="container-fluid">
-//             <div class="row">
-//                 <div class= "col-xxl-3 col-md-6 col-xs-6 col-lg-4">
-//                     <img class="auto" src="${product.image}" alt="${product.name}">
-//                 </div>
-//                 <div class= "col-xxl-9 col-md-6 col-xs-6 col-lg-8">
-//                     <div class="informacion">
-//                         <p class="negrita">${product.name}</p>
-//                         <p>${product.description}</p>
-//                         <p>Vendidos: ${product.soldCount}</p>
-//                         <p class="precio">${product.currency} ${product.cost}</p>
-//                     </div>
-//                 </div>
-            
-//             </div>
-//         </div>`;
-
-            
-//             contenedorJson.appendChild(productoDiv);
-//         });
-//     })
-//     .catch(error => {
-//         console.error('Hubo un problema con la solicitud:', error);
-//     });
