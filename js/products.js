d1e7241a7a932e95146fa7205d3a4d422aec061e
camilapessina <capessinab@gmail.com>
2024-09-12 18:43:19 -0300

    seguimos con filtros

--- js/products.js
+++ js/products.js
@@ -1,49 +1,62 @@
-
-
 const ORDER_ASC_BY_COST = ">$";
 const ORDER_DESC_BY_COST = "$<";
 const ORDER_BY_PROD_COUNT = "Cant.";
 let currentProductsArray = [];
 let currentSortCriteria = undefined;
-let minCost = undefined;
-let maxCost = undefined;
 
+function sortProducts(criteria) {
+    if (!currentProductsArray || currentProductsArray.length === 0) return;
 
-function sortProducts(criteria, array){
-    let result = [];
+    let sortedProducts = [];
     if (criteria === ORDER_ASC_BY_COST) {
-    result = array.sort(function(a, b) {
-
-    return parseInt(a.cost) - parseInt(b.cost);
-});
+        sortedProducts = currentProductsArray.sort((a, b) => parseInt(a.cost) - parseInt(b.cost));
     } else if (criteria === ORDER_DESC_BY_COST) {
-    result = array.sort(function(a, b) {
+        sortedProducts = currentProductsArray.sort((a, b) => parseInt(b.cost) - parseInt(a.cost));
+    } else if (criteria === ORDER_BY_PROD_COUNT) {
+        sortedProducts = currentProductsArray.sort((a, b) => parseInt(a.soldCount) - parseInt(b.soldCount));
+    }
 
-    return parseInt(b.cost) - parseInt(a.cost);
-});
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-    result = array.sort(function(a, b) {
-        let aCount = parseInt(a.soldCount);
-        let bCount = parseInt(b.soldCount);
+    displayProducts(sortedProducts);
+}
 
-    return parseInt(b.soldCount) - parseInt(a.soldCount);   
-    }) 
-    }else {
-    
-    return result;
+function displayProducts(products) {
+    const contenedorJson = document.getElementById('contenedorJson');
+    contenedorJson.innerHTML = '';
 
-}};
-
+    products.forEach(product => {
+        const productoDiv = document.createElement('div');
+        productoDiv.className = 'producto';
+        
+        productoDiv.innerHTML = `
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xxl-3 col-md-6 col-xs-6 col-lg-4">
+                    <img class="auto" src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="col-xxl-9 col-md-6 col-xs-6 col-lg-8">
+                    <div class="informacion">
+                        <p class="negrita">${product.name}</p>
+                        <p>${product.description}</p>
+                        <p>Vendidos: ${product.soldCount}</p>
+                        <p class="precio">${product.currency} ${product.cost}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        
+        contenedorJson.appendChild(productoDiv);
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     const catID = localStorage.getItem('catID'); 
 
-    // if (!catID) {
-    //     console.error('No se encontró el identificador de categoría en localStorage.');
-    //     return; 
-    // }
+    if (!catID) {
+        console.error('No se encontró el identificador de categoría en localStorage.');
+        return; 
+    }
 
-const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
+    const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`;
 
     fetch(url)
         .then(response => {
@@ -53,32 +66,8 @@ const url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.jso
             return response.json();
         })
         .then(data => {
-            const contenedorJson = document.getElementById('contenedorJson');
-            contenedorJson.innerHTML = ''; 
-
-            data.products.forEach(product => {
-                const productoDiv = document.createElement('div');
-                productoDiv.className = 'producto';
-                
-                productoDiv.innerHTML = `
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class= "col-xxl-3 col-md-6 col-xs-6 col-lg-4">
-                            <img class="auto" src="${product.image}" alt="${product.name}">
-                        </div>
-                        <div class= "col-xxl-9 col-md-6 col-xs-6 col-lg-8">
-                            <div class="informacion">
-                                <p class="negrita">${product.name}</p>
-                                <p>${product.description}</p>
-                                <p>Vendidos: ${product.soldCount}</p>
-                                <p class="precio">${product.currency} ${product.cost}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
-                
-                contenedorJson.appendChild(productoDiv);
-            });
+            currentProductsArray = data.products;  // Guardar productos en la variable global
+            displayProducts(currentProductsArray); // Mostrar productos inicialmente
 
             document.getElementById("sortAsc").addEventListener("click", function(){
                 sortProducts(ORDER_ASC_BY_COST);
